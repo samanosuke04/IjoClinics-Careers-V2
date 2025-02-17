28b426d16a768030f470e574d337dffb87efe432
Ahmad Faizul <133996209+samanosuke04@users.noreply.github.com>
2025-02-17 05:33:14 +0000

    rendered dynamic data "JOBS" using templating, created an API route at "/api/jobs"

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {"title": "Data Analyst","location": "Bengaluru, India","salary": "MYR 4,000"},
+  {"title": "Data Scientist","location": "Delhi, India","salary": "MYR 3,000"},
+  {"title": "Frontend Engineer","location": "Remote","salary": "MYR 1,000"},
+  {"title": "Backend Engineer","location": "San Francisco, USA","salary": "USD 1,500"}
+]
 
 @app.route("/")
 def hello():
-  return "Hello, World!"
+  return render_template("main.html", jobs = JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
