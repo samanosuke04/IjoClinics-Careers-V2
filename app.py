946551af6a25a1102b8fbbb5f2228cd8d678030c
Ahmad Faizul <133996209+samanosuke04@users.noreply.github.com>
2025-02-21 03:09:23 +0000

    added a form, that connects succesfully to db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, save_form_to_db
 
 app = Flask(__name__)
 
@@ -12,6 +12,21 @@ def start_web():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_jobpage(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job = job)
+
+@app.route("/job/<id>/apply", methods = ["post"])
+def application_submitted(id):
+  data = request.form
+  job = load_job_from_db(id)
+  save_form_to_db(id, data)
+  return render_template('application_submitted.html', application = data, job=job)
+  
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -20,3 +20,19 @@ def load_jobs_from_db():
         for row in result:
             jobs.append(row._asdict())
         return jobs
+
+def load_job_from_db(job_id):
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT * FROM jobs WHERE id = :val'), {'val':job_id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
+
+def save_form_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO application (job_id, full_name, birthdate, address, resume_link) VALUES (:job_id, :full_name, :birthdate, :address, :resume_link)")
+        conn.execute(query, 
+                     {'job_id': job_id,'full_name': data['full_name'], 'birthdate': data['date_of_birth'], 'address': data['address'], 'resume_link': data['resume_link']})
+        conn.commit()
